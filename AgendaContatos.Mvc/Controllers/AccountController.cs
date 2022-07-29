1bbcc165120908fafb3ab6387e7de084bb16dc54
smendescoti <sergio.coti@gmail.com>
2022-07-28 21:58:22 -0300

    Cadastro de conta de usuário

--- AgendaContatos.Mvc/Controllers/AccountController.cs
+++ AgendaContatos.Mvc/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AgendaContatos.Data.Entities;
+using AgendaContatos.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AgendaContatos.Mvc.Controllers
 {
@@ -13,6 +15,32 @@ namespace AgendaContatos.Mvc.Controllers
         //ROTA: /Account/Register
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost] //Método para receber os dados enviados pelo formulário
+        public IActionResult Register(string nome, string email, string senha, string senhaConfirmacao)
+        {
+            try
+            {
+                var usuario = new Usuario();
+
+                usuario.IdUsuario = Guid.NewGuid();
+                usuario.Nome = nome;
+                usuario.Email = email;
+                usuario.Senha = senha;
+                usuario.DataCadastro = DateTime.Now;
+
+                var usuarioRepository = new UsuarioRepository();
+                usuarioRepository.Create(usuario);
+
+                TempData["Mensagem"] = $"Parabéns {usuario.Nome}, sua conta foi cadastrada com sucesso!";
+            }
+            catch (Exception e)
+            {
+                TempData["Mensagem"] = $"Falha ao cadastrar: {e.Message}";
+            }
+
             return View();
         }
 
