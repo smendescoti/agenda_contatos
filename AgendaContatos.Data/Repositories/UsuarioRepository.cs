a0dcfef601ec8b3979866b926ab04f35aeafe6c5
smendescoti <sergio.coti@gmail.com>
2022-08-02 22:02:05 -0300

    Aula AspNet MVC 02/08

--- AgendaContatos.Data/Repositories/UsuarioRepository.cs
+++ AgendaContatos.Data/Repositories/UsuarioRepository.cs
@@ -35,5 +35,39 @@ namespace AgendaContatos.Data.Repositories
                 connection.Execute(sql, usuario);
             }
         }
+
+        //método para consultar 1 usuário baseado no email
+        public Usuario GetByEmail(string email)
+        {
+            var sql = @"
+                SELECT * FROM USUARIO
+                WHERE EMAIL = @email
+            ";
+
+            using (var connection = new SqlConnection(SqlServerConfiguration.GetConnectionString()))
+            {
+                return connection
+                    .Query<Usuario>(sql, new { email })
+                    .FirstOrDefault();
+            }
+        }
+
+        //método para consultar 1 usuário baseado no email e senha
+        public Usuario GetByEmailAndSenha(string email, string senha)
+        {
+            var sql = @"
+                SELECT * FROM USUARIO
+                WHERE EMAIL = @email
+                AND SENHA = CONVERT(VARCHAR(32), HASHBYTES('MD5', @senha), 2)
+            ";
+
+            using (var connection = new SqlConnection(SqlServerConfiguration.GetConnectionString()))
+            {
+                return connection
+                    .Query<Usuario>(sql, new { email, senha })
+                    .FirstOrDefault();
+            }
+        }
+
     }
 }
